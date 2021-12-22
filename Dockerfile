33f25198c1b10561479af455ec3392451562511f
Wendell Sun <iwendellsun@gmail.com>
2021-12-22 19:20:00 +0800

    add flags

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
-FROM golang:1.17.5-alpine3.14
+FROM golang:1.17.5-alpine3.15 AS builder
 Add . /go/src/completion-gen
 WORKDIR /go/src/completion-gen
-
 RUN go build ./cmd/completion-gen
+
+FROM alpine:3.15.0
+WORKDIR /workspace
+COPY --from=builder /go/src/completion-gen/completion-gen /workspace
+COPY --from=builder /go/src/completion-gen/tmpls /workspace
+ENTRYPOINT ["/workspace/completion-gen"]
+
--- cmd/completion-gen/main.go
+++ cmd/completion-gen/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	gen "github.com/xwjdsh/completion-gen"
 )
 
+var (
+	fp      = flag.String("w", "", "write file path")
+	command = flag.String("c", "", "command")
+)
+
 func main() {
-	if err := gen.Gen("docker", os.Stdout); err != nil {
+	flag.Parse()
+
+	if *command == "" {
+		panic("command not set")
+	}
+	writer := os.Stdout
+	if *fp != "" {
+		f, err := os.Create(*fp)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		writer = f
+	}
+
+	if err := gen.Gen(*command, writer); err != nil {
 		panic(err)
 	}
 }
